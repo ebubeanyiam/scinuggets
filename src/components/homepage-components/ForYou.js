c5bb272e7ae0745d68f9f59956ae11577bc73d9e
ebubeanyiam <dbupanyiam@gmail.com>
2021-01-09 08:05:45 +0100

    .

--- src/components/homepage-components/ForYou.js
+++ src/components/homepage-components/ForYou.js
@@ -106,7 +106,7 @@ const ForYou = ({ trend, setCustom }) => {
             ))}
         </div>
 
-        <div>
+        <div className="homepage__custom-posts-container">
           {postSliced &&
             postSliced.map((post, index) => (
               <Link key={index} to={post.data().slug}>
@@ -149,27 +149,52 @@ const ForYou = ({ trend, setCustom }) => {
         </div>
 
         <div className="homepage__custom-posts-writers">
-          <h1 className="homepage__custom-posts--marker">WRITERS TO FOLLOW</h1>
-          <div className="homepage__custom-posts--writer-box-container">
-            {authors.length !== 0 &&
-              authors.map((author, index) => (
-                <div className="homepage__custom-posts--writer-box" key={index}>
-                  <div className="homepage__custom-posts--writer-box-bio">
-                    {author.data().photoUrl && (
-                      <img src={author.data().photoUrl} alt="writer" />
-                    )}
-                    <div className="homepage__custom-posts--writer-box-bio-about">
-                      {author.data().displayName && (
-                        <h5>{author.data().displayName}</h5>
+          <div className="homepage__custom-posts-writers-child">
+            <h1 className="homepage__custom-posts--marker">
+              WRITERS TO FOLLOW
+            </h1>
+            <div className="homepage__custom-posts--writer-box-container">
+              {authors.length !== 0 &&
+                authors.map((author, index) => (
+                  <div
+                    className="homepage__custom-posts--writer-box"
+                    key={index}
+                  >
+                    <div className="homepage__custom-posts--writer-box-bio">
+                      {author.data().photoUrl && (
+                        <img src={author.data().photoUrl} alt="writer" />
                       )}
-                      {author.data().bio && <span>{author.data().bio}</span>}
+                      <div className="homepage__custom-posts--writer-box-bio-about">
+                        {author.data().displayName && (
+                          <h5>{author.data().displayName}</h5>
+                        )}
+                        {author.data().bio && <span>{author.data().bio}</span>}
+                      </div>
+                    </div>
+                    <div className="homepage__custom-posts--writer-box-follow">
+                      <button>Follow</button>
                     </div>
                   </div>
-                  <div>
-                    <button>Follow</button>
-                  </div>
-                </div>
-              ))}
+                ))}
+            </div>
+          </div>
+
+          <div className="homepage__custom-posts-writers-child">
+            <h1 className="homepage__custom-posts--marker">TOPICS TO FOLLOW</h1>
+            <div className="homepage__custom-posts--topics-container">
+              <div className="homepage__custom-posts--topic-box">
+                <h3>Coronavirus</h3>
+                <button>Follow</button>
+              </div>
+              <div className="homepage__custom-posts--topic-box">
+                <h3>Programming</h3>
+                <button>Follow</button>
+              </div>
+              <div className="homepage__custom-posts--topic-box">
+                <h3>Javascript</h3>
+                <button>Follow</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
